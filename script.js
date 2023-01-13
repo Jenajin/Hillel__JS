284a9b8af24d96f040b20c444bf08a89ffab211b
Andrii <Zefey@ukr.net>
2023-01-13 18:45:16 +0200

    test

--- script.js
+++ script.js
@@ -1,33 +1,7 @@
-const minus = '-';
-const plus = '+';
-const mnozhennya = '*';
-const dilennya = '/';
+let fruit = prompt("Які фрукти купити?", "apple");
 
-let nameAction = prompt('Що ви хочете зробити?', 'dev, sub, mult, div');
+let bag = {
+  [fruit]: 5, // назву властивості взято зі змінної fruit
+};
 
-let number1 = prompt('Введіть перше число');
-let number2 = prompt('Введіть дрге число число');
-
-if (nameAction == 'dev') {
-    alert(Number(number1) - Number(number2));
-} else if (nameAction == minus) {
-    alert(Number(number1) - Number(number2));
-} else
-
-if (nameAction == 'sub') {
-    alert(Number(number1) + Number(number2));
-} else if (nameAction == plus) {
-    alert(Number(number1) + Number(number2));
-} else
-
-if (nameAction == 'mult') {
-    alert(Number(number1) * Number(number2));
-} else if (nameAction == mnozhennya) {
-    alert(Number(number1) * Number(number2));
-} else
-
-if (nameAction == 'div' | '/') {
-    alert(Number(number1) / Number(number2));
-} else if (nameAction == dilennya) {
-    alert(Number(number1) / Number(number2));
-}  
+alert( bag.apple ); // 5 якщо fruit="apple"
