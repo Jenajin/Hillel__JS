890441ecbce06fa9c1c1154045bebfe16c745cf3
Andrii <Zefey@ukr.net>
2023-01-15 22:57:57 +0200

    finished

--- script.js
+++ script.js
@@ -1,7 +1,35 @@
-let fruit = prompt("Які фрукти купити?", "apple");
+// Завдання 1
+for (let pace = 20; pace <= 30; pace += 0.5) {
+  console.log(pace);
+}
 
-let bag = {
-  [fruit]: 5, // назву властивості взято зі змінної fruit
-};
+// Завдання 2
+let dollar = 27;
+for (let price = 10; price <= 100; price += 10) {
+  let dollar = 27 * price;
+  console.log(dollar);
+}
 
-alert( bag.apple ); // 5 якщо fruit="apple"
+// Завдання 3
+let N = 50;
+for (let i = 1; i <= 100; i++) {
+  if (i ** 2 < N) {
+    console.log(i);
+  }
+}
+
+// Завдання 4
+let simple = 5;
+for (let range = 2; range <= 2; range++) {
+  if (simple % range !== 0) {
+    console.log(simple);
+  }
+}
+
+// Завдання 5
+let arbitrary = 9;
+for (let I = 0; I < arbitrary; I++) {
+  if (3 ** I == arbitrary) {
+    console.log('можна');
+  }
+}  
