5cc4f15ec42c3e145def86aaae5a9724c2cd8c08
Andrii <Zefey@ukr.net>
2023-01-23 16:04:46 +0200

    fd

--- script.js
+++ script.js
@@ -1,41 +1,138 @@
-// Завдання 1
-for (let pace = 20; pace <= 30; pace += 0.5) {
-console.log(pace);
-}
+let arr = [16, -37, 54, -4, 72, -56, 47, 4, -16, 25, -37, 46, 4, -51, 27, -63, 4, -54, 76, -4, 12, -35, 4, 47];
 
-// Завдання 2
-let dollar = 27;
-for (let price = 10; price <= 100; price += 10) {
-  let dollar = 27 * price;
-  console.log(dollar);
-}
+// ЗАВДАННЯ 1. Знайти суму та кількість позитивних елементів.
+let positiveEl = arr.filter((el) => {
+  if (el > 0) {
+    return el;
+  }
+})
 
-// Завдання 3
-let N = 50;
-for (let i = 1; i <= 100; i++) {
-  if (i ** 2 < N) {
-    console.log(i);
+let sumPositiveEl = positiveEl.reduce((acummulator, item) => acummulator + item);
+console.log(`Сума позитивних елементів = ${sumPositiveEl}, кількість позитивних елементів = ${positiveEl.length}`);
+
+
+// ЗАВДАННЯ 2. Знайти мінімальний елемент масиву та його порядковий номер.
+let minNumb = arr[0];
+let i = 0;
+while (i < arr.length) {
+  if (minNumb > arr[i]) {
+    minNumb = arr[i]
+  }
+  i++;
+}
+console.log(`Мінімальний елемент масиву = ${minNumb}, його порядковий номер = ${arr.indexOf(minNumb) + 1}`);
+
+
+// ЗАВДАННЯ 3. Знайти максимальний елемент масиву та його порядковий номер.
+let maxNumb = arr[0];
+for (let i = 0; i < arr.length; i++) {
+  if (maxNumb < arr[i]) {
+    maxNumb = arr[i]
   }
 }
+console.log(`Максимальний елемент масиву = ${maxNumb}, його порядковий номер ${arr.indexOf(maxNumb) + 1}`);
 
-// Завдання 4
-let simple = 5;
-for (let range = 2; range <= 2; range++) {
-  if (simple % range !== 0) {
-    console.log(simple);
+
+// ЗАВДАННЯ 4. Визначити кількість негативних елементів.
+let negativeEl = arr.filter((el) => {
+  if (el < 0) {
+    return el;
   }
-}
+})
+console.log(`Кількість негативних елементів = ${negativeEl.length}`);
 
-// Завдання 5
-let arbitrary = 9;
-for (let I = 0; I < arbitrary; I++ ) {
-  if (3 ** I == arbitrary) {
-    console.log('можна');
+
+// ЗАВДАННЯ 5. Знайти кількість непарних позитивних елементів.
+
+// знаходимо числа які повторюються
+let twinEl = arr.filter((el, index, selfArr) => {
+  return selfArr.indexOf(el) !== index;
+})
+
+// відфільтровуємо з первинного масиву елементи які вкючає в себе масив twinEl (повторювані числа)
+let oddEl = arr.filter((el) => !twinEl.includes(el)
+)
+
+// відфільтровуємо позитивні елементи із уже унікальних
+let oddPositiveEl = oddEl.filter((el) => {
+  if (el > 0) {
+    return el;
   }
+})
+console.log(`Кількість непарних позитивних елементів = ${oddPositiveEl.length}`)
+
+
+// ЗАВДАННЯ 6. Знайти кількість парних позитивних елементів.
+let twinPositiveEl = twinEl.filter((el) => {
+  if (el > 2) {
+    return el;
+  }
+})
+console.log(`Кількість парних позитивних елементів = ${twinPositiveEl.length}`);
+
+
+// ЗАВДАННЯ 7. Знайти суму парних позитивних елементів.
+let sumTwinPositiveEl = twinPositiveEl.reduce((acummulator, item) => acummulator + item);
+console.log(`Cума парних позитивних елементів = ${sumTwinPositiveEl}`);
+
+
+// ЗАВДАННЯ 8. Знайти суму непарних позитивних елементів.
+let sumOddPositiveElEl = oddPositiveEl.reduce((acummulator, item) => acummulator + item);
+console.log(`Cума непарних позитивних елементів = ${sumOddPositiveElEl}`);
+
+
+// ЗАВДАННЯ 9. Знайти добуток позитивних елементів.
+
+// перший спосіб вирішення
+let product = positiveEl.reduce((acummulator, item) => acummulator * item);
+console.log(product);
+
+// другий спосіб вирішення
+let productCycle = 1;
+for (let i = 0; i < positiveEl.length; i++) {
+  productCycle = productCycle * positiveEl[i]; // або  productCycle *= positiveEl[i];
 }
+console.log(productCycle);
 
- 
 
+// ЗАВДАННЯ 10. Знайти найбільший серед елементів масиву, остальні обнулити.
+
+// знаходимо найбільший елемент серед масиву
+let maxNumberArr = arr[0];
+arr.forEach((el) => {
+  if (maxNumberArr < el) {
+    maxNumberArr = el
+  }
+})
+
+// обнуляємо елементи окрім найбільшого
+let arrNev = arr.map((el) => {
+  if (arr.indexOf(el) === 18) { // 18 індекс найбільшого числа
+    return maxNumberArr;
+  } else {
+    return 0;
+  }
+})
+console.log(arrNev)
+
+// або
+
+let arrNull = [];
+arr.forEach((el) => {
+  if (arr.indexOf(el) !== 18) { // 18 індекс найбільшого числа
+    arrNull.push(0)
+  } else {
+    arrNull.push(maxNumberArr)
+  }
+})
+console.log(arrNull)
+
+// або
+
+let arrTr = arr.splice(0, 18) && arr.splice(1, 6); // можна через splice добавити нулі замість вирізаних;
+arr.unshift(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,);
+arr.push(0, 0, 0, 0, 0,);
+console.log(arr);
 
 
 
