0ea3f3a26e43c445bc5b6d51cc5bd710bc942a1b
Andrii <Zefey@ukr.net>
2023-01-29 21:35:22 +0200

    добавив умови

--- script.js
+++ script.js
@@ -1,21 +1,36 @@
+// ЗАВДАННЯ. Реалізуйте функцію removeElement(array, item), щоб видалити елемент item з масиву array.
+
+
 // функція видаляє елемент із масиву знаходячи цей елемент по індексу
 let array = [1, 2, 3, 4, 5, 6, 7];
 function removeElement(array, indexItem) {
-    array.splice(indexItem, 1);
-}
-removeElement(array, 4);
-console.log(array);
-
-
-// функція видаляє елемент із масиву знаходячи цей елемент по назві
-let array2 = [1, 2, 3, 4, 5, 6, 7, 'дракон'];
-function removeElement2(array, nameItem) {
-    let nevArr = [];
-    for (i = 0; i <= array.length - 1; i++) {
-        if (array[i] !== nameItem) {
-            nevArr.push(array[i])
-        }
+    if (indexItem <= array.length - 1) {     // якщо немає елемента з таким індекс повертає false
+        return array.splice(indexItem, 1);
     }
-    return nevArr;
 }
-console.log(removeElement2(array2, 'дракон'));
+
+if (removeElement(array, 6)) {
+    console.log(array)
+} else (
+    console.log('не має')
+)
+
+
+
+// функція видаляє елемент із масиву знаходячи цей елемент по назві за умови що він є у масиві 
+let array2 = [1, 2, 3, 4, 5, 6, 7, 'JS'];
+function removeElement2(array, nameItem) {
+    if (array.includes(nameItem)) {
+        let nevArr = [];
+        for (i = 0; i <= array.length - 1; i++) {
+            if (array[i] !== nameItem) {
+                nevArr.push(array[i])
+            }
+        }
+        return nevArr;
+    }
+    else {
+        return 'такого елемента не має в масиві'
+    }
+}
+console.log(removeElement2(array2, 'др'));
