0a27053557bef426f676a128d3cbb8c804958b17
Andrii <Zefey@ukr.net>
2023-02-23 20:34:21 +0200

    ds

--- script.js
+++ script.js
@@ -1,160 +1,174 @@
-// ЗАВДАННЯ 1. Дано масив з елементами різних типів. Створити функцію яка вираховує 
-// середнє арифметичне лише числових елементів даного масиву.
+// ЗАВДАННЯ
 
-let arrFull = [1, 3, 4, 'місяць', 'день', true, false, 0, 7, , , 96, undefined];
+// Вам потрібно зробити конструктор сутності "Студент".
+// Студент має ім'я, прізвище, рік народження — це властивості. Є масив з оцінками, це також властивість. 
+// І є можливість отримати вік студента та його середній бал – це методи.
 
-function arithmeticMean(array) {
-  let arrNev = [];
-  array.forEach(el => {
-    if (typeof el === 'number') {
-      arrNev.push(el)
+// Ще у всіх Студентів є по масиву однакової довжини, у ньому 25 елементів, спочатку він не заповнений, 
+// але на 25 елементів. Це масив, в якому відзначається відвідуваність, щоразу коли ми викликаємо метод .present() 
+// на чергове порожнє місце, в масив записується true, коли викликаємо .absent() - записується false. 
+// Передбачте будь-який захист від того, щоб у масиві відвідуваності не могло бути більше 25 записів. 
+// Масив – це властивість, present та absent – ​​методи.
+
+// Останній метод: .summary(), перевіряє середню оцінку і середнє відвідування(кількістьВідвідин/кількістьЗанять), 
+// і якщо середня оцінка більше 90, а середнє відвідування більше 0.9, то метод summary повертає рядок "Молодець!", 
+// якщо одне з цих значень менше , то - "Добре, але можна краще ", якщо обидва нижче - "Редиска!".
+
+// Не забудьте після того, як напишите цей конструктор, створити 2-3 екземпляри (конкретних студентів) 
+// і показати використання цих методів.
+
+
+class Students {
+    constructor(name, lastName, yearOfBirth, arrEva) {
+        this.name = name
+        this.lastName = lastName
+        this.yearOfBirth = yearOfBirth
+        this.arrEva = arrEva
+        this.attendance = 26
+        this.arrBase = []
     }
 
-  });
-  let arrSum = arrNev.reduce((acummulator, item) => acummulator + item)
-  return arrSum / arrNev.length;
-}
-
-const result = arithmeticMean(arrFull)
-console.log(result)
-
-
-// ЗАВДАННЯ 2. Написати функцію doMath(x, znak, y), яка отримує 3 аргументи: числа x і y, рядок znak. 
-// У змінній znak може бути: +, -, *, /, %, ^ (ступінь ).Вивести результат математичної дії, 
-// вказаної в змінній znak. Обидва числа і знак виходять від користувача.
-
-function askOne(questionNum) {
-  const x = prompt(questionNum);
-  if(isNaN(x)) {
-    return askOne(`Це не число. Попробуйте ще раз.`)
-  } 
-  return x
-}
-const x = askOne(('Введіть перше число'));
-console.log(x)
-
-function action(question) {
-  let znak = prompt(question);
-  let result = (znak === '+' || znak === '-' || znak === '*' || znak === '/'
-    || znak === '%' || znak === '^') ? znak : action(`Вибрана недопустима дія. Попробуйте ще раз.`);
-  return result;
-}
-const znak = action(('Виберіть дію'));
-console.log(znak)
-
-function askTwo(questionNum) {
-  const y = prompt(questionNum);
-  if(isNaN(y)) {
-    return askTwo(`Це не число. Попробуйте ще раз.`)
-  } 
-  return y
-}
-const y = askTwo(('Введіть друге число'));
-console.log(y)
-
-function doMath(x, znak, y) {
-  if (znak === '+') {
-    return Number(x) + Number(y)
-  } else if (znak === '-') {
-    return Number(x) - Number(y)
-  } else if (znak === '*') {
-    return Number(x) * Number(y)
-  } else if (znak === '/') {
-    return Number(x) / Number(y)
-  } else if (znak === '^') {
-    return Number(x) ** Number(y)
-  } else if (znak === '%') {
-    return Number(x) % Number(y)
-  } else if (typeof x === 'string') {
-    doMath(x, znak, y)
-  }
-}
-
-let resultCalc = doMath(x, znak, y)
-prompt(`Результат:`, resultCalc)
-
-
-/// ЗАВДАННЯ 3. Написати функцію заповнення даними користувача двомірного масиву. 
-// Довжину основного масиву і внутрішніх масивів задає користувач. 
-// Значення всіх елементів всіх масивів задає користувач.
-
-function filling(question) {
-  let questionBaseLength = prompt(question)
-  if (isNaN(questionBaseLength)) {
-    return filling('Будь ласка, введіть число')
-  }
-  const arrBase = new Array(Number(questionBaseLength))
-
-  let arTime = [];
-  function sum(questionTwo) {
-    let questionOneFill = prompt(questionTwo)
-    arTime.push(questionOneFill)
-
-    if (arTime.length === arrBase.length) {
-      return arTime
-    } else {
-      sum(questionTwo)
-    }
-  }
-  sum('Введіть значення для головного масиву')
-  for (i = 0; i < arrBase.length; i++) {
-    arrBase[i] = arTime[i]
-  }
-
-  function nestedArrOne(questionOne) {
-    let questionOneLength = prompt(questionOne)
-    if (isNaN(questionOneLength)) {
-      return nestedArrOne('Будь ласка, введіть число')
-    }
-    let questionOneFill = prompt('Введіть одне значення для першого вложеного масиву')
-    const arrOne = new Array(Number(questionOneLength))
-    return arrOne.fill(questionOneFill)
-  }
-  let resultNestedArrOne = nestedArrOne('Введіть довжину першого вложеного масиву')
-  arrBase.push(resultNestedArrOne)
-
-  function nestedArrTwo(questionTwo) {
-    let questionTwoLength = prompt(questionTwo)
-    if (isNaN(questionTwoLength)) {
-      return nestedArrTwo('Будь ласка, введіть число')
-    }
-    let questionTwoFill = prompt('Введіть одне значення для другого вложеного масиву')
-    const arrTwo = new Array(Number(questionTwoLength))
-    return arrTwo.fill(questionTwoFill)
-  }
-  let resultNestedArrTwo = nestedArrTwo('Введіть довжину другого вложеного масиву')
-  arrBase.push(resultNestedArrTwo)
-
-  return arrBase
-}
-
-let resultFill = filling('Введіть довжину для головного масиву')
-console.log(resultFill)
-
-
-// ЗАВДАННЯ 4. Створити функцію, яка прибирає з рядка всі символи, які ми передали другим аргументом. 
-// 'func(" hello world", ['l', 'd'])' поверне нам "heo wor". Вихідний рядок та символи для видалення задає користувач.
-
-function deleteLetters() {
-  let str = prompt('Введіть речення', 'hello world');
-  let sym = prompt('Введіть букви для видалення', '   l,    d   ');
-
-  function deleteEl(str, sym) {
-    let nevArrStr = [];
-    for (i = 0; i < str.length; i++) {
-      nevArrStr.push(str[i])
+    present() {
+        if (this.arrBase.length === this.attendance) {
+            return this.arrBase
+        }
+        this.arrBase.push(true)
     }
 
-    let nevArrSym = [];
-    for (i = 0; i < sym.length; i++) {
-      nevArrSym.push(sym[i].trim())
+    absent() {
+        if (this.arrBase.length === this.attendance) {
+            return this.arrBase
+        }
+        this.arrBase.push(false)
     }
 
-    let result = nevArrStr.filter((el) => !nevArrSym.includes(el))
-    return alert(result.join(''))
-  }
+    age() {
+        console.log( `Студенту ${this.name} ${this.lastName} ${2023 - this.yearOfBirth} років`)
+    }
 
-  return deleteEl(str, sym)
+    gpa() {
+        const average = this.arrEva.reduce((acummulator, item) => acummulator + item) / this.arrEva.length
+        this.average = Math.round(average) 
+        console.log(`Середня значення оцінки ${this.name} ${this.lastName} складає ${this.average}`) 
+    }
+
+    summary() {
+        this.counter = 0
+        
+        for (let el of this.arrBase) {
+            if (el === true) {
+                this.counter++
+            }
+        }
+
+        if (this.average > 90 && (this.counter * 100 / this.arrBase.length) > 90) {
+            alert('Молодець!')
+        } else if (this.average > 90 || this.counter > 3) {
+            alert('Добре, але можна краще.')
+        } else {
+            alert('Редиска!')
+        }
+    }
 }
 
-deleteLetters()
+
+// Застосування конструкторної сутності "Студент"
+
+// 1 відвідування занять і оцінки на найвищому рівні
+const student1 = new Students('Mark', 'Bern', 1984, [100, 87, 100, 100, 100, 92, 100])
+student1.age()
+student1.gpa()
+
+student1.present()
+student1.present()
+student1.present()
+student1.absent()
+student1.present()
+student1.present()
+student1.present()
+student1.present()
+student1.present()
+student1.present()
+student1.present()
+student1.present()
+student1.present()
+student1.present()
+student1.present()
+student1.present()
+student1.present()
+student1.present()
+student1.present()
+student1.present()
+
+student1.summary()
+
+console.log(`Молодець`, student1)
+
+
+// 2 оцінки на найвищому рівні проте відвідування занять на середньому
+const student2 = new Students('Jack', 'Gregar', 1994, [100, 87, 50, 50, 100, 100, 100])
+student2.age()
+student2.gpa()
+
+student2.absent()
+student2.absent()
+student2.absent()
+student2.absent()
+student2.present()
+student2.present()
+student2.present()
+student2.present()
+student2.present()
+student2.present()
+student2.present()
+student2.present()
+student2.present()
+student2.present()
+student2.present()
+student2.present()
+
+
+student2.summary()
+
+console.log(`Хорошист`, student2)
+
+// 3 погані оцінки та погана відвідуваність
+const student3 = new Students('Andrii', 'Bern', 1994, [100, 87, 50, 50, 100, 100, 100])
+student3.age()
+student3.gpa()
+student2.absent()
+student2.absent()
+student2.absent()
+student2.absent()
+student2.absent()
+student2.absent()
+student2.absent()
+student2.absent()
+student2.absent()
+student2.absent()
+student2.absent()
+student2.absent()
+student2.absent()
+student2.absent()
+student2.absent()
+student2.absent()
+student2.absent()
+student2.absent()
+student2.absent()
+student2.absent()
+student2.absent()
+student2.absent()
+student2.absent()
+student2.absent()
+student2.absent()
+student2.absent()
+student2.absent()
+student2.absent()
+student2.absent()
+student2.absent()
+student2.absent()
+student2.absent()
+
+student3.summary()
+
+console.log(`Редиска!`, student3)
