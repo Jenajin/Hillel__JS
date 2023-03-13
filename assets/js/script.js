872dc0c9f84bbd6194d1332a52b8ef23390321d5
Andrii <Zefey@ukr.net>
2023-03-13 15:12:13 +0200

    false exporn import

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,115 @@
+const dairyProducts = [
+    {
+        name: 'milk',
+        description: 'Молоко корисне для травної системи',
+        price: 20,
+    },
+    {
+        name: 'sour cream',
+        description: 'Це не сметанний продукт',
+        price: 40,
+    },
+    {
+        name: 'kefir',
+        description: 'Містить корисні біфідобактерії',
+        price: 50,
+    },
+    {
+        name: 'cheese',
+        description: 'Свіжий',
+        price: 60,
+    },
+    {
+        name: 'cream',
+        description: 'Можна додавати до кави',
+        price: 70,
+    },
+]
+
+const fruits = [
+    {
+        name: 'banana',
+        description: 'Багато білка',
+        price: 63,
+    },
+    {
+        name: 'apple',
+        description: 'Сприяє кращому травленню',
+        price: 87,
+    },
+    {
+        name: 'pineapple',
+        description: 'Екзотичний фрукт',
+        price: 120,
+    },
+    {
+        name: 'coconut',
+        description: 'З нього роблять кокосову стружку',
+        price: 48,
+    },
+    {
+        name: 'orange',
+        description: 'Експортований з півдня',
+        price: 10,
+    },
+]
+
+const vegetables = [
+    {
+        name: 'Капуста',
+        description: 'Багато білка',
+        price: 80,
+    },
+    {
+        name: 'Часник',
+        description: 'Їсти тільки навечір',
+        price: 54,
+    },
+    {
+        name: 'Цибуля',
+        description: 'Дуже пекуча',
+        price: 90,
+    },
+    {
+        name: 'Морква',
+        description: 'Промита, очищена',
+        price: 100,
+    },
+    {
+        name: 'Помідор',
+        description: 'Помідор зелений',
+        price: 30,
+    },
+]
+
+const meatProducts = [
+    {
+        name: 'Ковбаса',
+        description: 'Дрогобицька, свіжа',
+        price: 500,
+    },
+    {
+        name: 'Рулет',
+        description: 'Зроблений із телячого фаршу',
+        price: 300,
+    },
+    {
+        name: 'Крильця',
+        description: 'Курячі, стероїдні',
+        price: 300,
+    },
+    {
+        name: 'Сало',
+        description: 'Посолене, перчене, проварене',
+        price: 200,
+    },
+    {
+        name: 'Філе куряче',
+        description: 'Багато білка',
+        price: 300,
+    },
+]
+
 const getEl = (selector) => document.querySelector(selector);
 
 function internetShop() {
